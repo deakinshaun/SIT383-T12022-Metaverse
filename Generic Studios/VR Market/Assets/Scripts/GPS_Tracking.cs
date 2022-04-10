9849146701662d0fc0ef7d3019766e655bcf3c1d
ChunyiWang <wangchuny@deakin.edu.au>
2022-04-10 19:57:43 +1000

    Update: component 1 update

--- Generic Studios/VR Market/Assets/Scripts/GPS_Tracking.cs
+++ Generic Studios/VR Market/Assets/Scripts/GPS_Tracking.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class GPS_Tracking : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool retrieveLocation(out float latitude, out
+        float longitude, out float altitude)
     {
-        
-    }
+        latitude = 0.0f;
+        longitude = 0.0f;
+        altitude = 0.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("Location service needs to be enabled");
+            return false;
+        }
+        if (Input.location.status != LocationServiceStatus.Running)
+        {
+            Debug.Log("Starting location service");
+            if (Input.location.status == LocationServiceStatus.Stopped)
+            {
+                Input.location.Start();
+            }
+            return false;
+        }
+        else
+        {
+            latitude = Input.location.lastData.latitude;
+            longitude = Input.location.lastData.longitude;
+            altitude = Input.location.lastData.altitude;
+            return true;
+        }
     }
 }
