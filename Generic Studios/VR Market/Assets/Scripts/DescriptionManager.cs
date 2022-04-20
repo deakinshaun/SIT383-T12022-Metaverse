c026c8c9f113302ccc6950f5d61726d7f0a1a6dc
Daniel Joshua Agbay <80764810+dagbay@users.noreply.github.com>
2022-04-21 01:40:03 +1000

    [Generic Studios - Daniel] Added Component 1

---
+++ Generic Studios/VR Market/Assets/Scripts/DescriptionManager.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DescriptionManager : MonoBehaviour
+{
+    public string myString;
+    public TextMeshProUGUI myText;
+    public float fadeTime = 3f;
+    public bool displayInfo;
+    [SerializeField]
+    private GameObject pedestal;
+    // private Vector3 pos;
+    // public string textObjName;
+
+    // Use this for initialization
+    void Start()
+    {
+        myText.color = Color.clear;
+        // pos = pedestal.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        FadeText();
+
+    }
+
+
+    void FadeText()
+
+    {
+
+
+        if (displayInfo)
+        {
+            myText.text = myString;
+            myText.color = Color.Lerp(myText.color, Color.black, fadeTime * Time.deltaTime);
+            // myText.transform.position = new Vector3(pos.x,pos.y,pos.z);
+        }
+        else
+        {
+
+            myText.color = Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
+        }
+
+
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            displayInfo = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        displayInfo = false;
+    }
+
+}
+
