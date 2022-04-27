a07788cbb6abdf152f6654afa25772b90398a4ec
Danny Nguyen <69789289+dannynguyen57@users.noreply.github.com>
2022-04-27 23:38:01 +1000

    [RSO META} update joystick (movement)
    update for Interaction and Feedback components

--- RSO Meta Group/RSO Meta/Assets/Scripts/EyesCam.cs
+++ RSO Meta Group/RSO Meta/Assets/Scripts/EyesCam.cs
@@ -6,6 +6,7 @@ public class EyesCam : MonoBehaviour
 {
     public Material camTextMaterial;
     private WebCamTexture webcamTexture;
+    
     // Start is called before the first frame update
     void Start()
     {
--- RSO Meta Group/RSO Meta/Assets/Scripts/MoveAvatar.cs
+++ RSO Meta Group/RSO Meta/Assets/Scripts/MoveAvatar.cs
@@ -8,6 +8,8 @@ public class MoveAvatar : MonoBehaviour
     public float moveSpeed = 2.0f;
     public float turnSpeed = 30.0f;
     public float playerJumpHeight = 2.0f;
+    public Joystick joystick;
+   
     void Start()
     {
         transform.position = new Vector3(0, 2.6f, 0);
@@ -17,8 +19,12 @@ public class MoveAvatar : MonoBehaviour
         if ((GetComponent <PhotonView> ().IsMine) ||
             (!PhotonNetwork.IsConnected))
         {
-            float move = Input.GetAxis("Vertical");
-            float turn = Input.GetAxis("Horizontal");
+            
+           // float move = Input.GetAxis("Vertical");
+           // float turn = Input.GetAxis("Horizontal");
+            float move = joystick.Vertical;
+            float turn = joystick.Horizontal;
+           // move = joystick.Horizontal;
 
             transform.rotation *= Quaternion.AngleAxis(
                 turn * turnSpeed * Time.deltaTime,
