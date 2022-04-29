b0714b48066a43a2d0dffd7e3cefceb45daeb5dc
dagbay <80764810+dagbay@users.noreply.github.com>
2022-04-29 23:33:23 +1000

    [Generic Studios - Daniel] Improved Hand Model Animations, Action, and Input

--- Generic Studios/VR Market/Assets/HandPresencePhysics.cs
+++ Generic Studios/VR Market/Assets/HandPresencePhysics.cs
@@ -6,15 +6,34 @@ public class HandPresencePhysics : MonoBehaviour
 {
     public Transform target;
     private Rigidbody rb;
+    public Renderer nonPhysicalHand;
+    public float showNonPhysicalHandDistance = 0.05f;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update() {
+        float distance = Vector3.Distance(transform.position, target.position);
+        if (distance>showNonPhysicalHandDistance) {
+            nonPhysicalHand.enabled = true; 
+        } else {
+            nonPhysicalHand.enabled = false;
+        }
+    }
+
+    void FixedUpdate()
     {
-        
+        // Position
+        rb.velocity = (target.position - transform.position)/Time.fixedDeltaTime;
+
+        // Rotation
+        Quaternion rotationDifference = target.rotation * Quaternion.Inverse(transform.rotation);
+        rotationDifference.ToAngleAxis(out float angleInDegree, out Vector3 rotationAxis);
+
+        Vector3 rotationDifferenceInDegree = angleInDegree * rotationAxis;
+
+        rb.angularVelocity = (rotationDifferenceInDegree * Mathf.Deg2Rad / Time.fixedDeltaTime);
     }
 }
