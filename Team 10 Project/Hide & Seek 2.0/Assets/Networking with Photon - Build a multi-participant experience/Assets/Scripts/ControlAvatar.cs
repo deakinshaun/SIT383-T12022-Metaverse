e48a7ba2744f000eb84a0a2bec8883aad8a6394c
MaiLinh2407 <linh.hoangngocmai@gmail.com>
2022-05-10 12:15:19 +1000

    Added Pun2 and set up multi-participants system.

--- Team 10 Project/Hide & Seek 2.0/Assets/Networking with Photon - Build a multi-participant experience/Assets/Scripts/ControlAvatar.cs
+++ Team 10 Project/Hide & Seek 2.0/Assets/Networking with Photon - Build a multi-participant experience/Assets/Scripts/ControlAvatar.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Photon.Pun;
 
 public class ControlAvatar : MonoBehaviour
 {
@@ -9,6 +11,23 @@ public class ControlAvatar : MonoBehaviour
     public float turnSpeed = 100.0f;
     public float moveSpeed = 10.0f;
 
+    void Start()
+    {
+        if (GetComponent <PhotonView> ().IsMine == true || PhotonNetwork.IsConnected == false)
+        {
+            GameObject.Find("Canvas/LeftButton").GetComponent<EventTrigger>().triggers[0].callback.AddListener((data) => { turnLeft(); });
+            GameObject.Find("Canvas/LeftButton").GetComponent<EventTrigger>().triggers[1].callback.AddListener((data) => { stopEverything(); });
+
+            GameObject.Find("Canvas/RightButton").GetComponent<EventTrigger>().triggers[0].callback.AddListener((data) => { turnRight(); });
+            GameObject.Find("Canvas/RightButton").GetComponent<EventTrigger>().triggers[1].callback.AddListener((data) => { stopEverything(); });
+
+            GameObject.Find("Canvas/ForwardButton").GetComponent<EventTrigger>().triggers[0].callback.AddListener((data) => { moveForward(); });
+            GameObject.Find("Canvas/ForwardButton").GetComponent<EventTrigger>().triggers[1].callback.AddListener((data) => { stopEverything(); });
+
+            GameObject.Find("Canvas/BackwardButton").GetComponent<EventTrigger>().triggers[0].callback.AddListener((data) => { moveBackward(); });
+            GameObject.Find("Canvas/BackwardButton").GetComponent<EventTrigger>().triggers[1].callback.AddListener((data) => { stopEverything(); });
+        }
+    }
     void Update()
     {
         transform.rotation *= Quaternion.AngleAxis(turn * turnSpeed * Time.deltaTime, Vector3.up);
