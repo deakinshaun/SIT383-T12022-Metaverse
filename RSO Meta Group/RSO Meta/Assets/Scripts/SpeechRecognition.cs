f80c9850ff7b6185f5f7ef8f2773bc8f7b3fdba2
Shizuru1 <33472020+Shizuru1@users.noreply.github.com>
2022-05-11 16:09:31 +1000

    Speech to Text update

--- RSO Meta Group/RSO Meta/Assets/Scripts/SpeechRecognition.cs
+++ RSO Meta Group/RSO Meta/Assets/Scripts/SpeechRecognition.cs
@@ -126,10 +126,6 @@ public class SpeechRecognition : MonoBehaviour
         yield return new WaitForSeconds(recordDuration);
         Microphone.End(null);
 
-        AudioSource audioSource = GetComponent<AudioSource>();
-        audioSource.clip = audio;
-        audioSource.Play();
-
         byte[] wavData = ConvertToWav(audio);
         SpeechToText(wavData);
     }
@@ -139,20 +135,20 @@ public class SpeechRecognition : MonoBehaviour
         StartCoroutine(recordAudio());
     }
 
-    const int HEADER_SIZE = 44;
+    /*const int HEADER_SIZE = 44;
 
     static byte[] ConvertToWav(AudioClip clip)
     {
         var samples = new float[clip.samples];
         clip.GetData(samples, 0);
-        Int16[] intData = new Int16[samples.Length];
-        Byte[] bytesData = new Byte[HEADER_SIZE + samples.Length * 2];
+        ushort[] intData = new ushort[samples.Length];
+        byte[] bytesData = new byte[HEADER_SIZE + samples.Length * 2];
         int rescaleFactor = 32767;
         WriteHeader(bytesData, clip);
         for (int i = 0; i<samples.Length; i++)
         {
-            intData[i] = (short)(samples[i] * rescaleFactor);
-            Byte[] byteArr = new Byte[2];
+            intData[i] = (ushort)(samples[i] * rescaleFactor);
+            byte[] byteArr = new byte[2];
             byteArr = BitConverter.GetBytes(intData[i]);
             byteArr.CopyTo(bytesData, HEADER_SIZE + i * 2);
         }
@@ -164,32 +160,113 @@ public class SpeechRecognition : MonoBehaviour
         var hz = clip.frequency;
         var channels = clip.channels;
         var samples = clip.samples;
+        byte[] riff = System.Text.Encoding.UTF8.GetBytes("RIFF");
+        riff.CopyTo(bytesData, 0);
+        byte[] chunkSize = BitConverter.GetBytes(HEADER_SIZE + clip.samples * 2 - 8);
+        chunkSize.CopyTo(bytesData, 4);
+        byte[] wave = System.Text.Encoding.UTF8.GetBytes("WAVE");
+        wave.CopyTo(bytesData, 8);
+        byte[] fmt = System.Text.Encoding.UTF8.GetBytes("fmt");
+        fmt.CopyTo(bytesData, 12);
+        byte[] subChunk1 = BitConverter.GetBytes(16);
+        subChunk1.CopyTo(bytesData, 16);
+        ushort one = 1;
+        byte[] audioFormat = BitConverter.GetBytes(one);
+        audioFormat.CopyTo(bytesData, 20);
+        byte[] numChannels = BitConverter.GetBytes(channels);
+        numChannels.CopyTo(bytesData, 22);
+        byte[] sampleRate = BitConverter.GetBytes(hz);
+        sampleRate.CopyTo(bytesData, 24);
+        byte[] byteRate = BitConverter.GetBytes(hz * channels * 2);
+        byteRate.CopyTo(bytesData, 28);
+        ushort blockAlign = (ushort)(channels * 2);
+        BitConverter.GetBytes(blockAlign).CopyTo(bytesData, 32);
+        ushort bps = 16;
+        byte[] bitsPerSample = BitConverter.GetBytes(bps);
+        bitsPerSample.CopyTo(bytesData, 34);
+        byte[] datastring = System.Text.Encoding.UTF8.GetBytes("data");
+        datastring.CopyTo(bytesData, 36);
+        byte[] subChunk2 = BitConverter.GetBytes(samples * 2);
+        subChunk2.CopyTo(bytesData, 40);
+    }*/
+
+    const int HEADER_SIZE = 44;
+
+    static byte[] ConvertToWav(AudioClip clip)
+    {
+
+        var samples = new float[clip.samples];
+
+        clip.GetData(samples, 0);
+
+        Int16[] intData = new Int16[samples.Length];
+        //converting in 2 float[] steps to Int16[], //then Int16[] to Byte[]
+
+        Byte[] bytesData = new Byte[HEADER_SIZE + samples.Length * 2];
+        //bytesData array is twice the size of
+        //dataSource array because a float converted in Int16 is 2 bytes.
+
+        int rescaleFactor = 32767; //to convert float to Int16
+
+        WriteHeader(bytesData, clip);
+
+        for (int i = 0; i < samples.Length; i++)
+        {
+            intData[i] = (short)(samples[i] * rescaleFactor);
+            Byte[] byteArr = new Byte[2];
+            byteArr = BitConverter.GetBytes(intData[i]);
+            byteArr.CopyTo(bytesData, HEADER_SIZE + i * 2);
+        }
+
+        return bytesData;
+    }
+
+    static void WriteHeader(byte[] bytesData, AudioClip clip)
+    {
+
+        var hz = clip.frequency;
+        var channels = clip.channels;
+        var samples = clip.samples;
+
         Byte[] riff = System.Text.Encoding.UTF8.GetBytes("RIFF");
         riff.CopyTo(bytesData, 0);
+
         Byte[] chunkSize = BitConverter.GetBytes(HEADER_SIZE + clip.samples * 2 - 8);
         chunkSize.CopyTo(bytesData, 4);
+
         Byte[] wave = System.Text.Encoding.UTF8.GetBytes("WAVE");
         wave.CopyTo(bytesData, 8);
-        Byte[] fmt = System.Text.Encoding.UTF8.GetBytes("fmt");
+
+        Byte[] fmt = System.Text.Encoding.UTF8.GetBytes("fmt ");
         fmt.CopyTo(bytesData, 12);
+
         Byte[] subChunk1 = BitConverter.GetBytes(16);
         subChunk1.CopyTo(bytesData, 16);
+
         UInt16 one = 1;
+
         Byte[] audioFormat = BitConverter.GetBytes(one);
         audioFormat.CopyTo(bytesData, 20);
+
         Byte[] numChannels = BitConverter.GetBytes(channels);
         numChannels.CopyTo(bytesData, 22);
+
         Byte[] sampleRate = BitConverter.GetBytes(hz);
         sampleRate.CopyTo(bytesData, 24);
-        Byte[] byteRate = BitConverter.GetBytes(hz * channels * 2);
+
+        Byte[] byteRate = BitConverter.GetBytes(hz * channels * 2); // sampleRate * bytesPerSample*number of channels, here 44100*2*2
         byteRate.CopyTo(bytesData, 28);
+
         UInt16 blockAlign = (ushort)(channels * 2);
         BitConverter.GetBytes(blockAlign).CopyTo(bytesData, 32);
+
         UInt16 bps = 16;
         Byte[] bitsPerSample = BitConverter.GetBytes(bps);
         bitsPerSample.CopyTo(bytesData, 34);
+
         Byte[] datastring = System.Text.Encoding.UTF8.GetBytes("data");
         datastring.CopyTo(bytesData, 36);
+
         Byte[] subChunk2 = BitConverter.GetBytes(samples * channels * 2);
         subChunk2.CopyTo(bytesData, 40);
     }
