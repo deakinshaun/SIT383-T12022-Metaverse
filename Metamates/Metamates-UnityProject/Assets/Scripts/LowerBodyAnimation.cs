99de000eafeb95b2b95b9da9f45e839e79babc08
Morgaine <mbarter@deakin.edu.au>
2022-05-20 23:38:38 +1000

    [Metamates]
    Lowerbody IK + animator script added

--- Metamates/Metamates-UnityProject/Assets/Scripts/LowerBodyAnimation.cs
+++ Metamates/Metamates-UnityProject/Assets/Scripts/LowerBodyAnimation.cs
@@ -4,6 +4,67 @@ using UnityEngine;
 
 public class LowerBodyAnimation : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
+
+    [SerializeField] [Range(0, 1)] private float leftFootPositionWeight;
+    [SerializeField] [Range(0, 1)] private float rightFootPositionWeight;
+
+    [SerializeField] [Range(0, 1)] private float leftFootRotationWeight;
+    [SerializeField] [Range(0, 1)] private float rightFootRotationWeight;
+
+    [SerializeField] private Vector3 footOffset;
+
+    [SerializeField] private Vector3 raycastLeftOffset;
+    [SerializeField] private Vector3 raycastRightOffset;
 
 
+    private void OnAnimatorIK(int layerIndex)
+    {
+        Vector3 leftFootPosition = animator.GetIKPosition(AvatarIKGoal.LeftFoot);
+        Vector3 rightFootPosition = animator.GetIKPosition(AvatarIKGoal.RightFoot);
+
+        RaycastHit hitLeftFoot;
+        RaycastHit hitRightFoot;
+
+        bool isLeftFootDown = Physics.Raycast(leftFootPosition + raycastLeftOffset, Vector3.down, out hitLeftFoot);
+        bool isRightFootDown = Physics.Raycast(rightFootPosition + raycastRightOffset, Vector3.down, out hitRightFoot);
+
+        CalculateLeftFoot(isLeftFootDown, hitLeftFoot);
+    }
+
+    void CalculateLeftFoot(bool isLeftFootDown, RaycastHit hitLeftFoot)
+    {
+        if (isLeftFootDown)
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, leftFootPositionWeight);
+            animator.SetIKPosition(AvatarIKGoal.LeftFoot, hitLeftFoot.point + footOffset);
+
+            Quaternion leftFootRotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(transform.forward, hitLeftFoot.normal), hitLeftFoot.normal);
+            animator.SetIKRotationWeight(AvatarIKGoal.LeftFoot, leftFootRotationWeight);
+            animator.SetIKRotation(AvatarIKGoal.LeftFoot, leftFootRotation);
+        }
+        else
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 0);
+
+        }
+    }
+
+    void CalculateRightFoot(bool isRightFootDown, RaycastHit hitRightFoot)
+    {
+        if (isRightFootDown)
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, rightFootPositionWeight);
+            animator.SetIKPosition(AvatarIKGoal.RightFoot, hitRightFoot.point + footOffset);
+
+            Quaternion rightFootRotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(transform.forward, hitRightFoot.normal), hitRightFoot.normal);
+            animator.SetIKRotationWeight(AvatarIKGoal.RightFoot, rightFootRotationWeight);
+            animator.SetIKRotation(AvatarIKGoal.RightFoot, rightFootRotation);
+        }
+        else
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, 0);
+
+        }
+    }
 }
