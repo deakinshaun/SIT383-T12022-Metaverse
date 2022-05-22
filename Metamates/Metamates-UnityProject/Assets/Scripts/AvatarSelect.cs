7dab941895e52c90b0c3f0b43425c0d1c05c0644
Morgaine <mbarter@deakin.edu.au>
2022-05-23 08:26:41 +1000

    [Metamates] Big red button
    testing avatar switch by pushing the big red button.
    script avatar select is in the Avatar Manager
    also tried to add a mug to the scene, still needs grabability

--- Metamates/Metamates-UnityProject/Assets/Scripts/AvatarSelect.cs
+++ Metamates/Metamates-UnityProject/Assets/Scripts/AvatarSelect.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class AvatarSelect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject Avatar;
+    public GameObject Avatar1;
+    public GameObject Avatar2;
+    public GameObject Avatar3;
+    public GameObject Avatar4;
+    public GameObject BigRedButton;
+    public bool pushed = false;
+
+    void SwitchAvatar()
     {
-        
+        //if big red button is pushed switch avatars
+        if (this.Avatar == Avatar1 && BigRedButton != pushed) //did i put this in wrong?
+        {
+            Avatar = Avatar2;
+        }
+        if (this.Avatar == Avatar2 && BigRedButton != pushed)
+        {
+            Avatar = Avatar3;
+        }
+        if (this.Avatar == Avatar3 && BigRedButton != pushed)
+        {
+            Avatar = Avatar4;
+        }
+        if (this.Avatar == Avatar4 && BigRedButton != pushed)
+        {
+            Avatar = Avatar1;
+        }
+
     }
 
     // Update is called once per frame
