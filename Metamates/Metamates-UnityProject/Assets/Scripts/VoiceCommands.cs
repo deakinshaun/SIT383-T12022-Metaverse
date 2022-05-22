1df929db6a831da01cedc47072f89dcc9da18ab0
Ben Monaghan <monoganog@gmail.com>
2022-05-23 08:15:36 +1000

    [Metamates] Building voice activated music controls

--- Metamates/Metamates-UnityProject/Assets/Scripts/VoiceCommands.cs
+++ Metamates/Metamates-UnityProject/Assets/Scripts/VoiceCommands.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class VoiceCommands : MonoBehaviour
 {
+
+    public AudioClip jazzMusic;
+    public AudioClip pianoMusic;
+    public AudioSource audioSource;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +24,26 @@ public class VoiceCommands : MonoBehaviour
     public void HelloWorld()
     {
         Debug.Log("Simple voice command activated. Hello World");
+    }
+
+    public void PlayJazz()
+    {
+        audioSource.clip = jazzMusic;
+        audioSource.Play();
+        Debug.Log("Now Playing Jazz Music");
+    }
+
+    public void PlayPiano()
+    {
+        audioSource.clip = pianoMusic;
+        audioSource.Play();
+        Debug.Log("Now Playing Piano Music");
+    }
+
+    public void StopMusic()
+    {
+        audioSource.Stop();
+        Debug.Log("Stopping Music");
+
     }
 }
