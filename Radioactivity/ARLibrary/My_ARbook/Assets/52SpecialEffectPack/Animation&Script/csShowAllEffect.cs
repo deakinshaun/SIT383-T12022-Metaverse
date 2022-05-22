0bc7c21fc71a106363ec69f798d7434906d766f2
YcZhang123456 <64247930+YcZhang123456@users.noreply.github.com>
2022-05-22 14:00:23 +0800

    AR LibraryFinal

---
+++ Radioactivity/ARLibrary/My_ARbook/Assets/52SpecialEffectPack/Animation&Script/csShowAllEffect.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class csShowAllEffect : MonoBehaviour
+{
+    public string[] EffectName;
+    public Transform[] Effect;
+    public int i = 0;
+
+    void Start()
+    {
+        Instantiate(Effect[i], new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    void Update ()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (i <= 0)
+                i = 51;
+
+            else
+                i--;
+
+           Instantiate(Effect[i], new Vector3(0, 0, 0), Quaternion.identity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (i < 51)
+                i++;
+
+            else
+                i = 0;
+
+            Instantiate(Effect[i], new Vector3(0, 0, 0), Quaternion.identity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        { 
+            Instantiate(Effect[i], new Vector3(0, 0, 0), Quaternion.identity);
+        }
+    }
+}
