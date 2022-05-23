0e520c0cc718bfcc49a7ff427cb3fb4a3b8c7f44
ReganCr <83275432+ReganCr@users.noreply.github.com>
2022-05-23 22:24:03 +1000

    Add files via upload

---
+++ RSO Meta Group/RSO Meta/Translate Library/Translate Library/UniLang/Example/Example.cs
@@ -0,0 +1,15 @@
+﻿using UniLang;
+using UnityEngine;
+
+namespace UniLang {
+    public class Example : MonoBehaviour {
+	    void Start () {
+            var myText = "Weather is great here";
+            Translator translator = Translator.Create(Language.Auto, Language.German);
+            translator.Run(myText, results => {
+                foreach (var result in results)
+                    Debug.Log(result.original + " => " + result.translated);
+            });
+	    }
+    }
+}
