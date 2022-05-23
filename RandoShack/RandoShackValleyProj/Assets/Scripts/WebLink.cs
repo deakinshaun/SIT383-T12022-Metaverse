5eb03fbac259e1057684af3e62c26be780185c26
NickWildish <ewtaylor@deakin.edu.au>
2022-05-24 01:14:02 +1000

    [Rando Shack] Added web link to app for video streaming
    Introduced a button that will direct users to the URL for video streaming

---
+++ RandoShack/RandoShackValleyProj/Assets/Scripts/WebLink.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebLink : MonoBehaviour
+{
+    public void OpenWebsite() {
+        Application.OpenURL("https://giant-honey-icon.glitch.me/streamvideo.html");
+    }
+}
